d1e7fe03742149771f6532dfa5c1c6bf637b0b72
ifs006 <ivanfs006@gmail.com>
2019-08-21 09:46:14 +0700

    rename regular_verb to RegularVerbActivity

--- app/src/main/java/com/example/gramairefacile/RegularVerbActivity.java
+++ app/src/main/java/com/example/gramairefacile/RegularVerbActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class regular_verb extends AppCompatActivity {
+public class RegularVerbActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
