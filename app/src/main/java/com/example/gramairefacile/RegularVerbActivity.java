fb8cb33dcabe8d13016c6f74597d4bfdd3fe8fe8
ifs006 <ivanfs006@gmail.com>
2019-08-21 09:56:36 +0700

    test commit

--- app/src/main/java/com/example/gramairefacile/RegularVerbActivity.java
+++ app/src/main/java/com/example/gramairefacile/RegularVerbActivity.java
@@ -1,22 +1,19 @@
 package com.example.gramairefacile;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class RegularVerbActivity extends AppCompatActivity {
 
+    private String title;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regular_verb);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
 
     }
 
