a482fe608a74d621742e7a15f0a8753d5f56af8f
ifs006 <ivanfs006@gmail.com>
2019-08-31 17:16:31 +0700

    Update styles

--- app/src/main/java/com/example/gramairefacile/activities/MenuAwalActivity.java
+++ app/src/main/java/com/example/gramairefacile/activities/MenuAwalActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
@@ -49,7 +48,10 @@ public class MenuAwalActivity extends AppCompatActivity
         ibtnLesAdjectif = (ImageButton) findViewById(R.id.ibtn_lesadjectif);
         ibtnLesConjonction = (ImageButton) findViewById(R.id.ibtn_lesconjonction);
         ibtnInterogatif = (ImageButton) findViewById(R.id.ibtn_interogatif);
+
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
         drawerToggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
@@ -108,28 +110,6 @@ public class MenuAwalActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menuawal, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
